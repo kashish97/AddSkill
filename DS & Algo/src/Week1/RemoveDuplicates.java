6a5c9deb1f02944a21d74af56e0a220074ac1e99
Kashish Gupta <kashishgup100@gmail.com>
2020-10-29 12:04:11 +0530

    added question of add two numbers

---
+++ DS & Algo/src/Week1/RemoveDuplicates.java
@@ -0,0 +1,23 @@
+package Week1;
+
+public class RemoveDuplicates {
+
+    public int removeDuplicates(int[] nums) {
+
+        //Total count of unique elements
+        int count = 1;
+        //index of where the next unique element is inserted
+        int j = 1;
+
+        for(int i = 0;i<nums.length-1;i++){
+            //checking if next element is not equal to the present one
+            if(nums[i]!=nums[i+1]){
+                nums[j]=nums[i+1];
+                j++;
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
